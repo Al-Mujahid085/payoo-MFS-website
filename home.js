982c330043770a4ed6aa8b6a9f1f38986bd0878c
Al Mujahid <amuzahid085@gmail.com>
2024-09-23 01:39:13 +0600

    final-commit

--- home.js
+++ home.js
@@ -1,3 +1,21 @@
+document.getElementById('show-cash-out-btn').addEventListener('click', function(event){
+    event.preventDefault();
+    document.getElementById("show-add-money-form").classList.add('hidden') 
+    document.getElementById("show-cash-out-form").classList.remove('hidden') 
+    document.getElementById('show-add-money-btn').style.backgroundColor = 'gray'
+    document.getElementById('show-cash-out-btn').style.backgroundColor = 'green'
+})
+document.getElementById('show-add-money-btn').addEventListener('click', function(event){
+    event.preventDefault();
+    document.getElementById('show-add-money-form').classList.remove('hidden')
+    document.getElementById('show-cash-out-form').classList.add('hidden')
+    document.getElementById('show-add-money-btn').style.backgroundColor = 'green'
+    document.getElementById('show-cash-out-btn').style.backgroundColor = 'gray'
+})
+
+
+
+
 document.getElementById('add-money').addEventListener('click', function(event){
     event.preventDefault();
     const cashAmount = document.getElementById('amout').value;
@@ -9,7 +27,24 @@ document.getElementById('add-money').addEventListener('click', function(event){
         const oldbalance = parseFloat(balance);
        const newBalance = addAmount + oldbalance;
        document.getElementById('balance').innerText = newBalance ;
-       console.log(newBalance)
-        
+       document.getElementById('amout').value ='';
+       document.getElementById('pin-number').value ='';
     }
 })
+
+document.getElementById('cash-out').addEventListener('click', function(event){
+    event.preventDefault();
+    const cashAmount = document.getElementById('amount').value;
+    const pin = document.getElementById('cashOut-pin-number').value;
+    const balance = document.getElementById('balance').innerText;
+    if(pin === '1234'){
+        const cashAmountNumber = parseFloat(cashAmount);
+        const balanceNumber = parseFloat(balance);
+        const newBalance =  balanceNumber - cashAmountNumber ;
+        document.getElementById('balance').innerText = newBalance;
+        document.getElementById('amount').value ='';
+        document.getElementById('cashOut-pin-number').value ='';
+
+    }
+})
+
